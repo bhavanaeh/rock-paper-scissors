55b359803c0aa4ae07b7d3d896663679b6197e1a
Bhavana <bhavana_sundar@srmuniv.edu.in>
2020-06-02 15:59:21 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+from keras_squeezenet import SqueezeNet
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D
+from keras.models import Sequential
+import tensorflow as tf
+import os
+
+IMG_SAVE_PATH = 'image_data'
+
+CLASS_MAP = {
+    "rock": 0,
+    "paper": 1,
+    "scissors": 2,
+    "none": 3
+}
+
+NUM_CLASSES = len(CLASS_MAP)
+
+
+def mapper(val):
+    return CLASS_MAP[val]
+
+
+def get_model():
+    model = Sequential([
+        SqueezeNet(input_shape=(227, 227, 3), include_top=False), #image size is 227x227 and 3 stands for the three channels RGB 
+        Dropout(0.5), #in order to prevent overfitting 50% dropout
+        Convolution2D(NUM_CLASSES, (1, 1), padding='valid'), #appending these few layers to the very end of the squeezenet NN
+        Activation('relu'), 
+        GlobalAveragePooling2D(), #performs the classification itself, calculates the avg op of each feature map in the prev layer i.e. data reduction layer
+        Activation('softmax') #probabilties of each layer
+    ])
+    return model
+
+
+# loading images
+dataset = []
+for directory in os.listdir(IMG_SAVE_PATH):
+    path = os.path.join(IMG_SAVE_PATH, directory)
+    if not os.path.isdir(path):
+        continue
+    for item in os.listdir(path):
+        # ensuring no hidden files get in the way
+        if item.startswith("."):
+            continue
+        img = cv2.imread(os.path.join(path, item))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (227, 227))
+        dataset.append([img, directory])
+
+'''
+dataset = [
+    [[...], 'rock'],
+    [[...], 'paper'],
+    ...
+]
+'''
+data, labels = zip(*dataset)
+labels = list(map(mapper, labels))
+
+
+'''
+labels: rock,paper,paper,scissors,rock...
+one hot encoded: [1,0,0], [0,1,0], [0,1,0], [0,0,1], [1,0,0]...
+'''
+
+# one hot encode the labels
+labels = np_utils.to_categorical(labels)
+
+# define the model
+model = get_model()
+model.compile(
+    optimizer=Adam(lr=0.0001),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# start training
+model.fit(np.array(data), np.array(labels), epochs=10)
+
+# save the model for later use
+model.save("rock-paper-scissors-model.h5")
